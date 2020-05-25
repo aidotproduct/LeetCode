0d36b4d41de4cf2ea36d607d5dd16c185279c90a
Milind Contractor <ai.dotproduct@gmail.com>
2020-05-25 16:22:40 +0800

    SingleNumber Class returns unique available value.

--- src/main/java/shreesai/com/array/ContainsDuplicateClass.java
+++ src/main/java/shreesai/com/array/ContainsDuplicateClass.java
@@ -10,10 +10,8 @@ public class ContainsDuplicateClass {
 
         for(int i = 0 ; i < nums.length; i++ )
         {
-            if(set.contains(nums[i]))
+            if(!set.add(nums[i]))
                 return true;
-            else
-                set.add(nums[i]);
         }
         return false;
     }
